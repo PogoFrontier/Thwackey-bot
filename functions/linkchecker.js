06065fcb42203acef980632ba46f70862c468b2e
theking465 <3542136-theking465@users.noreply.gitlab.com>
2021-01-01 12:12:11 +0100

    initial commit, used code from old bots

---
+++ functions/linkchecker.js
@@ -0,0 +1,37 @@
+module.exports = {
+    async execute(message) {
+        const Discord = require('discord.js');
+        const { bot, channels, roles } = require("../data.json");
+
+        //admins/moderators/minimods bypass link detection
+        if (message.member.roles.cache.has(roles.developer_ID)) {
+            return;
+        }
+
+        //see: https://regex101.com/r/rkT1y8/1/
+        let discordInvitePattern = /(discord\.(gg|io|me|li)|discord(app)?\.com\/invite)\/[a-z0-9]+/gi;
+
+        //check message
+        if (discordInvitePattern.test(message.content)) {
+            sendWarning();
+        }
+
+        function sendWarning() {
+            let embed = new Discord.MessageEmbed().setColor(bot.color).setTitle('Discord invite detected').setDescription('Posting discord invites without permission is not allowed, send a DM to an admin or moderator for approval.').setAuthor(message.member.user.tag).setTimestamp().setThumbnail(message.member.user.displayAvatarURL());
+
+            message.channel.send(embed);
+            message.delete();
+
+            let logChannel = message.guild.channels.cache.get(channels.log_ID);
+            let logembed = new Discord.MessageEmbed().setColor(bot.color).setTitle('Discord invite deleted').addField('message', message.content).setAuthor(message.member.user.tag).setTimestamp().setThumbnail(message.member.user.displayAvatarURL());
+
+            logChannel.send(logembed);
+        }
+
+        function sendApproval() {
+            let logChannel = message.guild.channels.cache.get(channels.log_ID);
+            let approvedEmbed = new Discord.MessageEmbed().setColor(bot.color).setTitle('Link approved').addField('message', message.content).addField("approved by", message.mentions.members.first().user.tag).setAuthor(message.member.user.tag).setTimestamp().setThumbnail(message.member.user.displayAvatarURL());
+            logChannel.send(approvedEmbed);
+        }
+    },
+};
