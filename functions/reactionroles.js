329d883d90b79022c77ec22a6e88e3fe3b56a857
theking465 <9497296+theking465@users.noreply.github.com>
2021-05-01 09:26:14 +0200

    added available role

--- functions/reactionroles.js
+++ functions/reactionroles.js
@@ -1,35 +1,36 @@
 module.exports = {
-	async execute() {
-		const client = require('../index.js').client;
-		const Discord = require('discord.js');
-		const { channels, messages, roles } = require('../data.json');
+    async execute() {
+        const client = require('../index.js').client;
+        const Discord = require('discord.js');
+        const { channels, messages, roles } = require('../data.json');
 
-		//fetches the message and guild
-		const reactionRolesMessage = await client.channels.cache.get(channels.roles_ID).messages.fetch(messages.reactionRoles_ID);
-		const guild = reactionRolesMessage.guild;
+        //fetches the message and guild
+        const reactionRolesMessage = await client.channels.cache.get(channels.roles_ID).messages.fetch(messages.reactionRoles_ID);
+        const guild = reactionRolesMessage.guild;
 
-		//adds a filter for the collector
-		const filter = (reaction) => {
-			return reaction.emoji.name == '🧑‍🎨' || reaction.emoji.name == '🧑‍💻' || reaction.emoji.name == '🎮' || reaction.emoji.name == '⚔️';
-		};
+        //adds a filter for the collector
+        const filter = (reaction) => {
+            return reaction.emoji.name == '🧑‍🎨' || reaction.emoji.name == '🧑‍💻' || reaction.emoji.name == '🎮' || reaction.emoji.name == '🟢';
+        };
 
-		//creates collector
-		const collector = new Discord.ReactionCollector(reactionRolesMessage, filter, { dispose: true });
+        //creates collector
+        const collector = new Discord.ReactionCollector(reactionRolesMessage, filter, { dispose: true });
 
-		//adds roles if collected emoji id matches
-		collector.on('collect', (reaction, user) => {
-			if (reaction.emoji.name == '🧑‍🎨') guild.members.cache.get(user.id).roles.add(roles.designer_ID);
-			if (reaction.emoji.name == '🧑‍💻') guild.members.cache.get(user.id).roles.add(roles.developer_ID);
-			if (reaction.emoji.name == '🎮') guild.members.cache.get(user.id).roles.add(roles.tester_ID);
-          
-		});
+        //adds roles if collected emoji id matches
+        collector.on('collect', (reaction, user) => {
+            if (reaction.emoji.name == '🧑‍🎨') guild.members.cache.get(user.id).roles.add(roles.designer_ID);
+            if (reaction.emoji.name == '🧑‍💻') guild.members.cache.get(user.id).roles.add(roles.developer_ID);
+            if (reaction.emoji.name == '🎮') guild.members.cache.get(user.id).roles.add(roles.tester_ID);
+            if (reaction.emoji.name == '🟢') guild.members.cache.get(user.id).roles.add(roles.available_ID);
+        });
 
-		//removes roles if collected emoji id matches
-		collector.on('remove', (reaction, user) => {
-			if (reaction.emoji.name == '🧑‍🎨') guild.members.cache.get(user.id).roles.remove(roles.designer_ID);
-			if (reaction.emoji.name == '🧑‍💻') guild.members.cache.get(user.id).roles.remove(roles.developer_ID);
-			if (reaction.emoji.name == '🎮') guild.members.cache.get(user.id).roles.remove(roles.tester_ID);
-          
-		});
-	},
+        //removes roles if collected emoji id matches
+        collector.on('remove', (reaction, user) => {
+            if (reaction.emoji.name == '🧑‍🎨') guild.members.cache.get(user.id).roles.remove(roles.designer_ID);
+            if (reaction.emoji.name == '🧑‍💻') guild.members.cache.get(user.id).roles.remove(roles.developer_ID);
+            if (reaction.emoji.name == '🎮') guild.members.cache.get(user.id).roles.remove(roles.tester_ID);
+            if (reaction.emoji.name == '🟢') guild.members.cache.get(user.id).roles.remove(roles.available_ID);
+
+        });
+    },
 };
